a53810189590841bde02c8448c9e05531266648c
Mariusz Bujny <mariusz.bujny@gmail.com>
2014-11-23 13:14:15 +0100

    Initial work with parallel managers

---
+++ parallelManagers/PetscParallelManager.h
@@ -0,0 +1,56 @@
+#ifndef _PETSC_PARALLEL_CONFIGURATION_H_
+#define _PETSC_PARALLEL_CONFIGURATION_H_
+
+#include "../Parameters.h"
+#include "../Definitions.h"
+#include <mpi.h>
+
+
+/** WS2.1.4. Class integrating reading (writing) from (to) parallel buffers.
+ */
+class PetscParallelManager {
+
+    private:
+
+        Parameters & _parameters;   //! Reference to the parameters
+
+        ParallelBoundaryIterator
+
+        /** Locates the six neighbors of the current process
+         */
+        void locateNeighbors();
+
+        /** Computes the indices of the current subdomain and stores the data in the parameters
+         */
+        void createIndices();
+
+        /** Returns the rank of the process with the indices provided
+         * @param i Intex in the X directon
+         * @param j Intex in the Y directon
+         * @param k Intex in the Z directon
+         * @return Rank of the process with that index, assuming that they are ordered
+         * lexicographically, or MPI_PROC_NULL if outside the domain
+         */
+        int computeRankFromIndices(int i, int j, int k) const;
+
+        /** Compute local sizes and sizes in all directions. Requires deallocation of sizes
+         */
+        void computeSizes();
+
+        /** Deletes the arrays allocated in the parameters. To be called in the destructor of this
+         * class
+         */
+        void freeSizes();
+
+    public:
+
+        /** Constructor
+         * @param parameters Reference to the parameters
+         */
+        PetscParallelManager(Parameters & parameters);
+
+        /** Destructor */
+        ~PetscParallelManager();
+};
+
+#endif
