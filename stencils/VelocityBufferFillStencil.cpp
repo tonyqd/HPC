0d82a95731b229f18dc1642bc6ea4d0f067e64f3
Matthias <matthias.thalhammer@googlemail.com>
2014-11-24 18:45:03 +0100

    VelocityStencils should work

--- stencils/VelocityBufferFillStencil.cpp
+++ stencils/VelocityBufferFillStencil.cpp
@@ -5,75 +5,75 @@ VelocityBufferFillStencil::VelocityBufferFillStencil( const Parameters & paramet
 
   int dim = parameters.geometry.dim;
 
-  leftVelocityFillBuffer = new FLOAT[(localSize[1]) * (localSize[2]) * dim + (localSize[2])];
-  rightVelocityFillBuffer = new FLOAT[(localSize[1]) * (localSize[2]) * dim + (localSize[1]) * (localSize[2])];
+  leftVelocityFillBuffer = new FLOAT[(localSize[1] + 2) * (localSize[2] + 2) * dim];
+  rightVelocityFillBuffer = new FLOAT[(localSize[1] + 2) * (localSize[2] + 2) * (dim + 1)];
 
-  bottomVelocityFillBuffer = new FLOAT[(localSize[0]) * (localSize[2]) * dim + (localSize[0]) * (localSize[2])];
-  topVelocityFillBuffer = new FLOAT[(localSize[0]) * (localSize[2]) * dim + (localSize[0]) * (localSize[2])];
+  bottomVelocityFillBuffer = new FLOAT[(localSize[0] + 2) * (localSize[2] + 2) * dim];
+  topVelocityFillBuffer = new FLOAT[(localSize[0] + 2) * (localSize[2] + 2) * (dim + 1)];
 
-  frontVelocityFillBuffer = new FLOAT[(localSize[0]) * (localSize[1]) * dim + (localSize[0]) * (localSize[1])];
-  backVelocityFillBuffer = new FLOAT[(localSize[0]) * (localSize[1]) * dim + (localSize[0]) * (localSize[1])];
+  frontVelocityFillBuffer = new FLOAT[(localSize[0] + 2) * (localSize[1] + 2) * dim];
+  backVelocityFillBuffer = new FLOAT[(localSize[0] + 2) * (localSize[1] + 2) * (dim + 1)];
 
 }
 
 
 
 void VelocityBufferFillStencil::applyLeftWall ( FlowField & flowField, int i, int j, int k) {
-  /* _lowOffset = 2; _highOffset = -1;
+  /* _lowOffset = 1; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
    */
-  leftVelocityFillBuffer[1 + 3 * ((k-2)+(localSize[1]*(j-2)))] = getVelocity().getVector(i, j, k)[0];
-  leftVelocityFillBuffer[2 + 3 * ((k-2)+(localSize[1]*(j-2)))] = getVelocity().getVector(i, j, k)[1];
-  leftVelocityFillBuffer[3 + 3 * ((k-2)+(localSize[1]*(j-2)))] = getVelocity().getVector(i, j, k)[2];
+  leftVelocityFillBuffer[1 + 3 * ((k-1)+(localSize[1]*(j-1)))] = getVelocity().getVector(i+1, j, k)[0];
+  leftVelocityFillBuffer[2 + 3 * ((k-1)+(localSize[1]*(j-1)))] = getVelocity().getVector(i+1, j, k)[1];
+  leftVelocityFillBuffer[3 + 3 * ((k-1)+(localSize[1]*(j-1)))] = getVelocity().getVector(i+1, j, k)[2];
 
 }
 
 void VelocityBufferFillStencil::applyRightWall ( FlowField & flowField, int i, int j, int k) {
-  /* _lowOffset = 2; _highOffset = -1;
+  /* _lowOffset = 1; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _flowField.getCellsX()+_highOffset-1 ; j = _lowOffset ; k = _lowOffset;
    */
-  rightVelocityFillBuffer[0 + 4*((k-2)+(localSize[1]*(j-2)))] = getVelocity().getVector(i,j,k)[0];
-  rightVelocityFillBuffer[1 + 4*((k-2)+(localSize[1]*(j-2)))] = getVelocity().getVector(i,j,k)[1];
-  rightVelocityFillBuffer[2 + 4*((k-2)+(localSize[1]*(j-2)))] = getVelocity().getVector(i,j,k)[2];
-  rightVelocityFillBuffer[3 + 4*((k-2)+(localSize[1]*(j-2)))] = getVelocity().getVector(i-1,j,k)[0];
+  rightVelocityFillBuffer[0 + 4*((k-1)+(localSize[1]*(j-1)))] = getVelocity().getVector(i-1,j,k)[0];
+  rightVelocityFillBuffer[1 + 4*((k-1)+(localSize[1]*(j-1)))] = getVelocity().getVector(i-1,j,k)[1];
+  rightVelocityFillBuffer[2 + 4*((k-1)+(localSize[1]*(j-1)))] = getVelocity().getVector(i-1,j,k)[2];
+  rightVelocityFillBuffer[3 + 4*((k-1)+(localSize[1]*(j-1)))] = getVelocity().getVector(i-2,j,k)[0];
 }
 
 void VelocityBufferFillStencil::applyBottomWall ( FlowField & flowField, int i, int j, int k) {
-  /* _lowOffset = 2; _highOffset = -1;
+  /* _lowOffset = 1; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
    */
-  bottomVelocityFillBuffer[0 + 3*((k-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[0];
-  bottomVelocityFillBuffer[1 + 3*((k-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[1];
-  bottomVelocityFillBuffer[2 + 3*((k-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[2];
+  bottomVelocityFillBuffer[0 + 3*((k-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j+1,k)[0];
+  bottomVelocityFillBuffer[1 + 3*((k-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j+1,k)[1];
+  bottomVelocityFillBuffer[2 + 3*((k-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j+1,k)[2];
 }
 
 void VelocityBufferFillStencil::applyTopWall ( FlowField & flowField, int i, int j, int k) {
-  /* _lowOffset = 2; _highOffset = -1;
+  /* _lowOffset = 1; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = Iterator<FlowField>::_flowField.getCellsY()+_highOffset-1 ; k = _lowOffset;
    */
-  topVelocityFillBuffer[0 + 4*((k-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[0];
-  topVelocityFillBuffer[1 + 4*((k-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[1];
-  topVelocityFillBuffer[2 + 4*((k-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[2];
-  topVelocityFillBuffer[3 + 4*((k-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j-1,k)[1];
+  topVelocityFillBuffer[0 + 4*((k-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j-1,k)[0];
+  topVelocityFillBuffer[1 + 4*((k-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j-1,k)[1];
+  topVelocityFillBuffer[2 + 4*((k-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j-1,k)[2];
+  topVelocityFillBuffer[3 + 4*((k-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j-2,k)[1];
 }
 
 void VelocityBufferFillStencil::applyFrontWall ( FlowField & flowField, int i, int j, int k) {
-  /* _lowOffset = 2; _highOffset = -1;
+  /* _lowOffset = 1; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
    */
-  backVelocityFillBuffer[0 + 4*((j-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[0];
-  backVelocityFillBuffer[1 + 4*((j-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[1];
-  backVelocityFillBuffer[2 + 4*((j-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[2];
+  frontVelocityFillBuffer[0 + 4*((j-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j,k+1)[0];
+  frontVelocityFillBuffer[1 + 4*((j-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j,k+1)[1];
+  frontVelocityFillBuffer[2 + 4*((j-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j,k+1)[2];
 }
 
 void VelocityBufferFillStencil::applyBackWall ( FlowField & flowField, int i, int j, int k) {
-  /* _lowOffset = 2; _highOffset = -1;
+  /* _lowOffset = 1; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = Iterator<FlowField>::_flowField.getCellsZ()+_highOffset-1 ;
    */
-  backVelocityFillBuffer[0 + 4*((j-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[0];
-  backVelocityFillBuffer[1 + 4*((j-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[1];
-  backVelocityFillBuffer[2 + 4*((j-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k)[2];
-  backVelocityFillBuffer[3 + 4*((j-2)+(localSize[0]*(i-2)))] = getVelocity().getVector(i,j,k-1)[1];
+  backVelocityFillBuffer[0 + 4*((j-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j,k-1)[0];
+  backVelocityFillBuffer[1 + 4*((j-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j,k-1)[1];
+  backVelocityFillBuffer[2 + 4*((j-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j,k-1)[2];
+  backVelocityFillBuffer[3 + 4*((j-1)+(localSize[0]*(i-1)))] = getVelocity().getVector(i,j,k-2)[2];
 }
 
 /*2D
---
+++ stencils/VelocityBufferReadStencil.cpp
@@ -0,0 +1,91 @@
+#include "VelocityBufferReadStencil.h"
+
+VelocityBufferReadStencil::VelocityBufferReadStencil( const Parameters & parameters):
+  BoundaryStencil<FlowField>(parameters), localSize(parameters.parallel.localSize) {
+
+  int dim = parameters.geometry.dim;
+
+  leftVelocityReadBuffer = new FLOAT[(localSize[1] + 2) * (localSize[2] + 2) * (dim + 1)];
+  rightVelocityReadBuffer = new FLOAT[(localSize[1] + 2) * (localSize[2] + 2) * dim];
+
+  bottomVelocityReadBuffer = new FLOAT[(localSize[0] + 2) * (localSize[2] + 2) * (dim + 1)];
+  topVelocityReadBuffer = new FLOAT[(localSize[0] + 2) * (localSize[2] + 2) * dim];
+
+  frontVelocityReadBuffer = new FLOAT[(localSize[0] + 2) * (localSize[1] + 2) * (dim + 1)];
+  backVelocityReadBuffer = new FLOAT[(localSize[0] + 2) * (localSize[1] + 2) * dim];
+
+}
+
+
+
+void VelocityBufferReadStencil::applyLeftWall ( FlowField & flowField, int i, int j, int k) {
+  /* _lowOffset = 1; _highOffset = 0;
+   * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
+   */
+  getVelocity().getVector(i, j, k)[0] = leftVelocityReadBuffer[0 + 4*((k-1)+(localSize[1]*(j-1)))];
+  getVelocity().getVector(i, j, k)[1] = leftVelocityReadBuffer[1 + 4*((k-1)+(localSize[1]*(j-1)))];
+  getVelocity().getVector(i, j, k)[2] = leftVelocityReadBuffer[2 + 4*((k-1)+(localSize[1]*(j-1)))];
+  getVelocity().getVector(i-1,j,k)[0] = leftVelocityReadBuffer[3 + 4*((k-1)+(localSize[1]*(j-1)))];
+
+}
+
+void VelocityBufferReadStencil::applyRightWall ( FlowField & flowField, int i, int j, int k) {
+  /* _lowOffset = 1; _highOffset = 0;
+   * from ParallelBoundaryIterater:  i = _flowField.getCellsX()+_highOffset-1 ; j = _lowOffset ; k = _lowOffset;
+   */
+  getVelocity().getVector(i, j, k)[0] = rightVelocityReadBuffer[0 + 3*((k-1)+(localSize[1]*(j-1)))];
+  getVelocity().getVector(i, j, k)[1] = rightVelocityReadBuffer[1 + 3*((k-1)+(localSize[1]*(j-1)))];
+  getVelocity().getVector(i, j, k)[2] = rightVelocityReadBuffer[2 + 3*((k-1)+(localSize[1]*(j-1)))];
+}
+
+void VelocityBufferReadStencil::applyBottomWall ( FlowField & flowField, int i, int j, int k) {
+  /* _lowOffset = 1; _highOffset = 0;
+   * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
+   */
+  getVelocity().getVector(i, j, k)[0] = bottomVelocityReadBuffer[0 + 4*((k-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i, j, k)[1] = bottomVelocityReadBuffer[1 + 4*((k-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i, j, k)[2] = bottomVelocityReadBuffer[2 + 4*((k-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i,j-1,k)[1] = bottomVelocityReadBuffer[3 + 4*((k-1)+(localSize[0]*(i-1)))];
+}
+
+void VelocityBufferReadStencil::applyTopWall ( FlowField & flowField, int i, int j, int k) {
+  /* _lowOffset = 1; _highOffset = 0;
+   * from ParallelBoundaryIterater:  i = _lowOffset ; j = Iterator<FlowField>::_flowField.getCellsY()+_highOffset-1 ; k = _lowOffset;
+   */
+  getVelocity().getVector(i, j, k)[0] = topVelocityReadBuffer[0 + 3*((k-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i, j, k)[1] = topVelocityReadBuffer[1 + 3*((k-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i, j, k)[2] = topVelocityReadBuffer[2 + 3*((k-1)+(localSize[0]*(i-1)))];
+}
+
+void VelocityBufferReadStencil::applyFrontWall ( FlowField & flowField, int i, int j, int k) {
+  /* _lowOffset = 1; _highOffset = 0;
+   * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
+   */
+  getVelocity().getVector(i, j, k)[0] = frontVelocityReadBuffer[0 + 4*((j-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i, j, k)[1] = frontVelocityReadBuffer[1 + 4*((j-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i, j, k)[2] = frontVelocityReadBuffer[2 + 4*((j-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i,j,k-1)[2] = frontVelocityReadBuffer[3 + 4*((j-1)+(localSize[0]*(i-1)))];
+}
+
+void VelocityBufferReadStencil::applyBackWall ( FlowField & flowField, int i, int j, int k) {
+  /* _lowOffset = 1; _highOffset = 0;
+   * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = Iterator<FlowField>::_flowField.getCellsZ()+_highOffset-1 ;
+   */
+  getVelocity().getVector(i, j, k)[0] = backVelocityReadBuffer[0 + 4*((j-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i, j, k)[1] = backVelocityReadBuffer[1 + 4*((j-1)+(localSize[0]*(i-1)))];
+  getVelocity().getVector(i, j, k)[2] = backVelocityReadBuffer[2 + 4*((j-1)+(localSize[0]*(i-1)))];
+}
+
+/*2D
+ */
+void VelocityBufferReadStencil::applyLeftWall ( FlowField & flowField, int i, int j) {
+}
+
+void VelocityBufferReadStencil::applyRightWall ( FlowField & flowField, int i, int j) {
+}
+
+void VelocityBufferReadStencil::applyBottomWall ( FlowField & flowField, int i, int j) {
+}
+
+void VelocityBufferReadStencil::applyTopWall ( FlowField & flowField, int i, int j) {
+}
