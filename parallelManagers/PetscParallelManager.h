d45efaf17371658e0c3e056653da3bf050dface5
Yunqing <eric.dong1234@gmail.com>
2014-12-11 17:44:13 +0100

    reduce communication for velocity

--- parallelManagers/PetscParallelManager.h
+++ parallelManagers/PetscParallelManager.h
@@ -123,32 +123,32 @@ void PetscParallelManager<FlowField>::communicateVelocities() {
 	_parallelVelocityFillBoundaryIterator.iterate();
 
 	// Copy from left to right
-	MPI_Send(_velocityBufferFillStencil.leftVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[1] + 3) * (_velocityBufferFillStencil.localSize[2] + 3) * dim), MY_MPI_FLOAT, _parameters.parallel.leftNb, 0, PETSC_COMM_WORLD);
-	MPI_Recv(_velocityBufferReadStencil.rightVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[1] + 3) * (_velocityBufferFillStencil.localSize[2] + 3) * dim), MY_MPI_FLOAT, _parameters.parallel.rightNb, 0, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_velocityBufferFillStencil.leftVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[1] + 2) * (_velocityBufferFillStencil.localSize[2] + 2) * 2), MY_MPI_FLOAT, _parameters.parallel.leftNb, 0, PETSC_COMM_WORLD);
+	MPI_Recv(_velocityBufferReadStencil.rightVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[1] + 2) * (_velocityBufferFillStencil.localSize[2] + 2) * 2), MY_MPI_FLOAT, _parameters.parallel.rightNb, 0, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	// Copy from right to left
-	MPI_Send(_velocityBufferFillStencil.rightVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[1] + 3) * (_velocityBufferFillStencil.localSize[2] + 3) * 2*(dim )), MY_MPI_FLOAT, _parameters.parallel.rightNb, 1, PETSC_COMM_WORLD);
-	MPI_Recv(_velocityBufferReadStencil.leftVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[1] + 3) * (_velocityBufferFillStencil.localSize[2] + 3) * 2*(dim )), MY_MPI_FLOAT, _parameters.parallel.leftNb, 1, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_velocityBufferFillStencil.rightVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[1] + 2) * (_velocityBufferFillStencil.localSize[2] + 2) *(dim )), MY_MPI_FLOAT, _parameters.parallel.rightNb, 1, PETSC_COMM_WORLD);
+	MPI_Recv(_velocityBufferReadStencil.leftVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[1] + 2) * (_velocityBufferFillStencil.localSize[2] + 2) *(dim )), MY_MPI_FLOAT, _parameters.parallel.leftNb, 1, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 	_parallelVelocityReadBoundaryIterator.iterate();
 
 	// Copy from bottom to top
 	_parallelVelocityFillBoundaryIterator.iterate();
-	MPI_Send(_velocityBufferFillStencil.bottomVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[0] + 3) * (_velocityBufferFillStencil.localSize[2] + 3) * dim), MY_MPI_FLOAT, _parameters.parallel.bottomNb, 2, PETSC_COMM_WORLD);
-	MPI_Recv(_velocityBufferReadStencil.topVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[0] + 3) * (_velocityBufferFillStencil.localSize[2] + 3) * dim), MY_MPI_FLOAT, _parameters.parallel.topNb, 2, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_velocityBufferFillStencil.bottomVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[0] + 2) * (_velocityBufferFillStencil.localSize[2] + 2) * 2), MY_MPI_FLOAT, _parameters.parallel.bottomNb, 2, PETSC_COMM_WORLD);
+	MPI_Recv(_velocityBufferReadStencil.topVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[0] + 2) * (_velocityBufferFillStencil.localSize[2] + 2) * 2), MY_MPI_FLOAT, _parameters.parallel.topNb, 2, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	// Copy from top to bottom
-	MPI_Send(_velocityBufferFillStencil.topVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[0] + 3) * (_velocityBufferFillStencil.localSize[2] + 3) * 2*(dim)), MY_MPI_FLOAT, _parameters.parallel.topNb, 3, PETSC_COMM_WORLD);
-	MPI_Recv(_velocityBufferReadStencil.bottomVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[0] + 3) * (_velocityBufferFillStencil.localSize[2] + 3) * 2*(dim )), MY_MPI_FLOAT, _parameters.parallel.bottomNb, 3, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_velocityBufferFillStencil.topVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[0] + 2) * (_velocityBufferFillStencil.localSize[2] + 2) * (dim)), MY_MPI_FLOAT, _parameters.parallel.topNb, 3, PETSC_COMM_WORLD);
+	MPI_Recv(_velocityBufferReadStencil.bottomVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[0] + 2) * (_velocityBufferFillStencil.localSize[2] + 2) * (dim )), MY_MPI_FLOAT, _parameters.parallel.bottomNb, 3, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 	_parallelVelocityReadBoundaryIterator.iterate();
 
 	// Copy from front to back
 	_parallelVelocityFillBoundaryIterator.iterate();
-	MPI_Send(_velocityBufferFillStencil.frontVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[0] + 3) * (_velocityBufferFillStencil.localSize[1] + 3) * dim), MY_MPI_FLOAT, _parameters.parallel.frontNb, 4, PETSC_COMM_WORLD);
-	MPI_Recv(_velocityBufferReadStencil.backVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[0] + 3) * (_velocityBufferFillStencil.localSize[1] + 3) * dim), MY_MPI_FLOAT, _parameters.parallel.backNb, 4, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_velocityBufferFillStencil.frontVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[0] + 2) * (_velocityBufferFillStencil.localSize[1] + 2) * 2), MY_MPI_FLOAT, _parameters.parallel.frontNb, 4, PETSC_COMM_WORLD);
+	MPI_Recv(_velocityBufferReadStencil.backVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[0] + 2) * (_velocityBufferFillStencil.localSize[1] + 2) * 2), MY_MPI_FLOAT, _parameters.parallel.backNb, 4, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	// Copy from back to front
-	MPI_Send(_velocityBufferFillStencil.backVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[0] + 3) * (_velocityBufferFillStencil.localSize[1] + 3) * 2*(dim )), MY_MPI_FLOAT, _parameters.parallel.backNb, 5, PETSC_COMM_WORLD);
-	MPI_Recv(_velocityBufferReadStencil.frontVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[0] + 3) * (_velocityBufferFillStencil.localSize[1] + 3) * 2*(dim )), MY_MPI_FLOAT, _parameters.parallel.frontNb, 5, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_velocityBufferFillStencil.backVelocityFillBuffer, ((_velocityBufferFillStencil.localSize[0] + 2) * (_velocityBufferFillStencil.localSize[1] + 2) *(dim )), MY_MPI_FLOAT, _parameters.parallel.backNb, 5, PETSC_COMM_WORLD);
+	MPI_Recv(_velocityBufferReadStencil.frontVelocityReadBuffer, ((_velocityBufferFillStencil.localSize[0] + 2) * (_velocityBufferFillStencil.localSize[1] + 2) * (dim )), MY_MPI_FLOAT, _parameters.parallel.frontNb, 5, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	_parallelVelocityReadBoundaryIterator.iterate();
 }
@@ -161,32 +161,32 @@ void PetscParallelManager<FlowField>::communicateViscosity() {
 	_parallelViscosityFillBoundaryIterator.iterate();
 
 	// Copy from left to right
-	MPI_Send(_viscosityBufferFillStencil.leftViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[1] + 3) * (_viscosityBufferFillStencil.localSize[2] + 3) ), MY_MPI_FLOAT, _parameters.parallel.leftNb, 0, PETSC_COMM_WORLD);
-	MPI_Recv(_viscosityBufferReadStencil.rightViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[1] + 3) * (_viscosityBufferFillStencil.localSize[2] + 3)), MY_MPI_FLOAT, _parameters.parallel.rightNb, 0, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_viscosityBufferFillStencil.leftViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[1]) * (_viscosityBufferFillStencil.localSize[2]) ), MY_MPI_FLOAT, _parameters.parallel.leftNb, 0, PETSC_COMM_WORLD);
+	MPI_Recv(_viscosityBufferReadStencil.rightViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[1]) * (_viscosityBufferFillStencil.localSize[2])), MY_MPI_FLOAT, _parameters.parallel.rightNb, 0, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	// Copy from right to left
-	MPI_Send(_viscosityBufferFillStencil.rightViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[1] + 3) * (_viscosityBufferFillStencil.localSize[2] + 3)), MY_MPI_FLOAT, _parameters.parallel.rightNb, 1, PETSC_COMM_WORLD);
-	MPI_Recv(_viscosityBufferReadStencil.leftViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[1] + 3) * (_viscosityBufferFillStencil.localSize[2] + 3) ), MY_MPI_FLOAT, _parameters.parallel.leftNb, 1, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_viscosityBufferFillStencil.rightViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[1]) * (_viscosityBufferFillStencil.localSize[2])), MY_MPI_FLOAT, _parameters.parallel.rightNb, 1, PETSC_COMM_WORLD);
+	MPI_Recv(_viscosityBufferReadStencil.leftViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[1]) * (_viscosityBufferFillStencil.localSize[2]) ), MY_MPI_FLOAT, _parameters.parallel.leftNb, 1, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 	_parallelViscosityReadBoundaryIterator.iterate();
 
 	// Copy from bottom to top
 	_parallelViscosityFillBoundaryIterator.iterate();
-	MPI_Send(_viscosityBufferFillStencil.bottomViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[0] + 3) * (_viscosityBufferFillStencil.localSize[2] + 3)), MY_MPI_FLOAT, _parameters.parallel.bottomNb, 2, PETSC_COMM_WORLD);
-	MPI_Recv(_viscosityBufferReadStencil.topViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[0] + 3) * (_viscosityBufferFillStencil.localSize[2] + 3) ), MY_MPI_FLOAT, _parameters.parallel.topNb, 2, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_viscosityBufferFillStencil.bottomViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[0]) * (_viscosityBufferFillStencil.localSize[2])), MY_MPI_FLOAT, _parameters.parallel.bottomNb, 2, PETSC_COMM_WORLD);
+	MPI_Recv(_viscosityBufferReadStencil.topViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[0]) * (_viscosityBufferFillStencil.localSize[2]) ), MY_MPI_FLOAT, _parameters.parallel.topNb, 2, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	// Copy from top to bottom
-	MPI_Send(_viscosityBufferFillStencil.topViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[0] + 3) * (_viscosityBufferFillStencil.localSize[2] + 3) ), MY_MPI_FLOAT, _parameters.parallel.topNb, 3, PETSC_COMM_WORLD);
-	MPI_Recv(_viscosityBufferReadStencil.bottomViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[0] + 3) * (_viscosityBufferFillStencil.localSize[2] + 3) ), MY_MPI_FLOAT, _parameters.parallel.bottomNb, 3, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_viscosityBufferFillStencil.topViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[0]) * (_viscosityBufferFillStencil.localSize[2]) ), MY_MPI_FLOAT, _parameters.parallel.topNb, 3, PETSC_COMM_WORLD);
+	MPI_Recv(_viscosityBufferReadStencil.bottomViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[0]) * (_viscosityBufferFillStencil.localSize[2]) ), MY_MPI_FLOAT, _parameters.parallel.bottomNb, 3, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 	_parallelViscosityReadBoundaryIterator.iterate();
 
 	// Copy from front to back
 	_parallelViscosityFillBoundaryIterator.iterate();
-	MPI_Send(_viscosityBufferFillStencil.frontViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[0] + 3) * (_viscosityBufferFillStencil.localSize[1] + 3) ), MY_MPI_FLOAT, _parameters.parallel.frontNb, 4, PETSC_COMM_WORLD);
-	MPI_Recv(_viscosityBufferReadStencil.backViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[0] + 3) * (_viscosityBufferFillStencil.localSize[1] + 3)), MY_MPI_FLOAT, _parameters.parallel.backNb, 4, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_viscosityBufferFillStencil.frontViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[0]) * (_viscosityBufferFillStencil.localSize[1]) ), MY_MPI_FLOAT, _parameters.parallel.frontNb, 4, PETSC_COMM_WORLD);
+	MPI_Recv(_viscosityBufferReadStencil.backViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[0]) * (_viscosityBufferFillStencil.localSize[1])), MY_MPI_FLOAT, _parameters.parallel.backNb, 4, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	// Copy from back to front
-	MPI_Send(_viscosityBufferFillStencil.backViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[0] + 3) * (_viscosityBufferFillStencil.localSize[1] + 3) ), MY_MPI_FLOAT, _parameters.parallel.backNb, 5, PETSC_COMM_WORLD);
-	MPI_Recv(_viscosityBufferReadStencil.frontViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[0] + 3) * (_viscosityBufferFillStencil.localSize[1] + 3)), MY_MPI_FLOAT, _parameters.parallel.frontNb, 5, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Send(_viscosityBufferFillStencil.backViscosityFillBuffer, ((_viscosityBufferFillStencil.localSize[0]) * (_viscosityBufferFillStencil.localSize[1]) ), MY_MPI_FLOAT, _parameters.parallel.backNb, 5, PETSC_COMM_WORLD);
+	MPI_Recv(_viscosityBufferReadStencil.frontViscosityReadBuffer, ((_viscosityBufferFillStencil.localSize[0]) * (_viscosityBufferFillStencil.localSize[1])), MY_MPI_FLOAT, _parameters.parallel.frontNb, 5, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	_parallelViscosityReadBoundaryIterator.iterate();
 }
--- stencils/VelocityBufferFillStencil.cpp
+++ stencils/VelocityBufferFillStencil.cpp
@@ -5,26 +5,25 @@ VelocityBufferFillStencil::VelocityBufferFillStencil( const Parameters & paramet
 
   int dim = parameters.geometry.dim;
 
-  leftVelocityFillBuffer = new FLOAT[(localSize[1] + 3) * (localSize[2] + 3) * dim];
-  rightVelocityFillBuffer = new FLOAT[(localSize[1] + 3) * (localSize[2] + 3) * dim*2 ];
+  leftVelocityFillBuffer = new FLOAT[(localSize[1] + 2) * (localSize[2] + 2) * 2];
+  rightVelocityFillBuffer = new FLOAT[(localSize[1] + 2) * (localSize[2] + 2) * dim ];
 
-  bottomVelocityFillBuffer = new FLOAT[(localSize[0] + 3) * (localSize[2] + 3) * dim];
-  topVelocityFillBuffer = new FLOAT[(localSize[0] + 3) * (localSize[2] + 3) * (dim )*2];
+  bottomVelocityFillBuffer = new FLOAT[(localSize[0] + 2) * (localSize[2] + 2) * 2];
+  topVelocityFillBuffer = new FLOAT[(localSize[0] + 2) * (localSize[2] + 2) * (dim )];
 
-  frontVelocityFillBuffer = new FLOAT[(localSize[0] + 3) * (localSize[1] + 3) * dim];
-  backVelocityFillBuffer = new FLOAT[(localSize[0] + 3) * (localSize[1] + 3) * (dim )*2];
+  frontVelocityFillBuffer = new FLOAT[(localSize[0] + 2) * (localSize[1] + 2) * 2];
+  backVelocityFillBuffer = new FLOAT[(localSize[0] + 2) * (localSize[1] + 2) * (dim )];
 
 }
 
 
 
 void VelocityBufferFillStencil::applyLeftWall ( FlowField & flowField, int i, int j, int k) {
-  /* _lowOffset = 0; _highOffset = 0;
+  /* _lowOffset = 1; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
    */
-  leftVelocityFillBuffer[0 + 3 * ((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i+2, j, k)[0];
-  leftVelocityFillBuffer[1 + 3 * ((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i+2, j, k)[1];
-  leftVelocityFillBuffer[2 + 3 * ((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i+2, j, k)[2];
+  leftVelocityFillBuffer[0 + 2* ((k)+((localSize[2]+2)*(j)))] = flowField.getVelocity().getVector(i+1, j, k)[1];
+  leftVelocityFillBuffer[1 + 2 * ((k)+((localSize[2]+2)*(j)))] = flowField.getVelocity().getVector(i+1, j, k)[2];
 
 }
 
@@ -34,54 +33,43 @@ void VelocityBufferFillStencil::applyRightWall ( FlowField & flowField, int i, i
    *for (int j = _lowOffset; j < Iterator<FlowField>::_flowField.getCellsY()+_highOffset; j++) {
     	for (int k = _lowOffset; k < Iterator<FlowField>::_flowField.getCellsZ()+_highOffset; k++)
    */
-  rightVelocityFillBuffer[0 + 6*((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i-2,j,k)[0];
-  rightVelocityFillBuffer[1 + 6*((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i-2,j,k)[1];
-  rightVelocityFillBuffer[2 + 6*((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i-2,j,k)[2];
-  rightVelocityFillBuffer[3 + 6*((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i-1,j,k)[0];
-  rightVelocityFillBuffer[4 + 6*((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i-1,j,k)[1];
-  rightVelocityFillBuffer[5 + 6*((k)+((localSize[2]+3)*(j)))] = flowField.getVelocity().getVector(i-1,j,k)[2];
+  rightVelocityFillBuffer[0 + 3*((k)+((localSize[2]+2)*(j)))] = flowField.getVelocity().getVector(i-2,j,k)[0];
+  rightVelocityFillBuffer[1 + 3*((k)+((localSize[2]+2)*(j)))] = flowField.getVelocity().getVector(i-1,j,k)[1];
+  rightVelocityFillBuffer[2 + 3*((k)+((localSize[2]+2)*(j)))] = flowField.getVelocity().getVector(i-1,j,k)[2];
 }
 
 void VelocityBufferFillStencil::applyBottomWall ( FlowField & flowField, int i, int j, int k) {
   /* _lowOffset = 0; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
    */
-  bottomVelocityFillBuffer[0 + 3*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j+2,k)[0];
-  bottomVelocityFillBuffer[1 + 3*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j+2,k)[1];
-  bottomVelocityFillBuffer[2 + 3*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j+2,k)[2];
+  bottomVelocityFillBuffer[0 + 2*((k)+((localSize[2]+2)*(i)))] = flowField.getVelocity().getVector(i,j+1,k)[0];
+  bottomVelocityFillBuffer[1 + 2*((k)+((localSize[2]+2)*(i)))] = flowField.getVelocity().getVector(i,j+1,k)[2];
 }
 
 void VelocityBufferFillStencil::applyTopWall ( FlowField & flowField, int i, int j, int k) {
   /* _lowOffset = 0; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = Iterator<FlowField>::_flowField.getCellsY()+_highOffset-1 ; k = _lowOffset;
    */
-  topVelocityFillBuffer[0 + 6*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j-2,k)[0];
-  topVelocityFillBuffer[1 + 6*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j-2,k)[1];
-  topVelocityFillBuffer[2 + 6*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j-2,k)[2];
-  topVelocityFillBuffer[3 + 6*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j-1,k)[0];
-  topVelocityFillBuffer[4 + 6*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j-1,k)[1];
-  topVelocityFillBuffer[5 + 6*((k)+((localSize[2]+3)*(i)))] = flowField.getVelocity().getVector(i,j-1,k)[2];
+  topVelocityFillBuffer[0 + 3*((k)+((localSize[2]+2)*(i)))] = flowField.getVelocity().getVector(i,j-1,k)[0];
+  topVelocityFillBuffer[1 + 3*((k)+((localSize[2]+2)*(i)))] = flowField.getVelocity().getVector(i,j-2,k)[1];
+  topVelocityFillBuffer[2 + 3*((k)+((localSize[2]+2)*(i)))] = flowField.getVelocity().getVector(i,j-1,k)[2];
 }
 
 void VelocityBufferFillStencil::applyFrontWall ( FlowField & flowField, int i, int j, int k) {
   /* _lowOffset = 0; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = _lowOffset;
    */
-  frontVelocityFillBuffer[0 + 3*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k+2)[0];
-  frontVelocityFillBuffer[1 + 3*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k+2)[1];
-  frontVelocityFillBuffer[2 + 3*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k+2)[2];
+  frontVelocityFillBuffer[0 + 2*((j)+((localSize[1]+2)*(i)))] = flowField.getVelocity().getVector(i,j,k+1)[0];
+  frontVelocityFillBuffer[1 + 2*((j)+((localSize[1]+2)*(i)))] = flowField.getVelocity().getVector(i,j,k+1)[1];
 }
 
 void VelocityBufferFillStencil::applyBackWall ( FlowField & flowField, int i, int j, int k) {
   /* _lowOffset = 0; _highOffset = 0;
    * from ParallelBoundaryIterater:  i = _lowOffset ; j = _lowOffset ; k = Iterator<FlowField>::_flowField.getCellsZ()+_highOffset-1 ;
    */
-  backVelocityFillBuffer[0 + 6*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k-2)[0];
-  backVelocityFillBuffer[1 + 6*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k-2)[1];
-  backVelocityFillBuffer[2 + 6*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k-2)[2];
-  backVelocityFillBuffer[3 + 6*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k-1)[0];
-  backVelocityFillBuffer[4 + 6*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k-1)[1];
-  backVelocityFillBuffer[5 + 6*((j)+((localSize[1]+3)*(i)))] = flowField.getVelocity().getVector(i,j,k-1)[2];
+  backVelocityFillBuffer[0 + 3*((j)+((localSize[1]+2)*(i)))] = flowField.getVelocity().getVector(i,j,k-1)[0];
+  backVelocityFillBuffer[1 + 3*((j)+((localSize[1]+2)*(i)))] = flowField.getVelocity().getVector(i,j,k-1)[1];
+  backVelocityFillBuffer[2 + 3*((j)+((localSize[1]+2)*(i)))] = flowField.getVelocity().getVector(i,j,k-2)[2];
 }
 
 /*2D
