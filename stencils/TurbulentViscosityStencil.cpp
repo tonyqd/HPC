8fa25e482bc9f599244cb3bab3fb26227e22cd4d
Matthias <matthias.thalhammer@googlemail.com>
2014-12-13 18:59:18 +0100

    different boundary layer models implemented

--- stencils/TurbulentViscosityStencil.cpp
+++ stencils/TurbulentViscosityStencil.cpp
@@ -42,12 +42,32 @@ void TurbulentViscosityStencil::apply ( FlowField & flowField,  int i, int j, in
 		 FLOAT S13 = dudz(_localVelocity, _localMeshsize) + dwdx(_localVelocity, _localMeshsize);
 		 FLOAT S23 = dvdz(_localVelocity, _localMeshsize) + dwdy(_localVelocity, _localMeshsize);
 
-		 FLOAT lm = _parameters.turbulent.kappa*flowField.getDistance().getScalar(i,j,k); //mixing length input
-		 //FLOAT delta = 1; //needs to be changed
+		 //default
+		 FLOAT lm = 0.0;
+		 FLOAT delta;
 
-		 //if(lm > 0.09*delta){
-		 //	 lm = 0.09*delta;
-		 //}
+		 //laminar reference case
+		 if(_parameters.turbulent.model=="standard"){
+			 lm = _parameters.turbulent.kappa*flowField.getDistance().getScalar(i,j,k);
+		 }
+
+		 //laminar flat plate Boundary layer
+		 if(_parameters.turbulent.model=="laminar"){
+			 lm = _parameters.turbulent.kappa*flowField.getDistance().getScalar(i,j,k);
+			 delta = 0.09*4.91*pow((_parameters.geometry.lengthX*(1.0-_parameters.bfStep.xRatio))/_parameters.flow.Re,0.5)*pow(_parameters.meshsize->getPosX(i,j,k) + _parameters.meshsize->getDx(i,j,k),0.5);
+			 if(lm > delta){
+				 lm = delta;
+			 }
+		 }
+
+		 //turbulent flat plate Boundary layer
+		 if(_parameters.turbulent.model=="turbulent"){
+			 lm = _parameters.turbulent.kappa*flowField.getDistance().getScalar(i,j,k);
+			 delta = 0.09*0.382*pow((_parameters.geometry.lengthX*(1.0-_parameters.bfStep.xRatio))/_parameters.flow.Re,0.2)*pow(_parameters.meshsize->getPosX(i,j,k) + _parameters.meshsize->getDx(i,j,k),0.8);
+			 if(lm > delta){
+				 lm = delta;
+			 }
+		 }
 
 		 flowField.getViscosity().getScalar(i,j,k) = lm * lm * sqrt(2.0*((S11*S11 + S22*S22 + S33*S33)+2.0*(S12*S12 + S13*S13 + S23*S23)));
 
